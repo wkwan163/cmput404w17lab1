551f6daa1ad804cdb62e8488444c0e8a56773aa1
wkwan163 <wkwan@ualberta.ca>
2017-01-10 09:04:05 -0700

    Python script with code that redirects to raw script on git repository

--- checkversion.py
+++ checkversion.py
@@ -4,7 +4,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/wkwan163/cmput404w17lab1/master/checkversion.py")
 
 print response.status_code
 
